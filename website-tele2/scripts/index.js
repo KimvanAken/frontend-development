23300942af438030a1f43e779abde4a6ca5c3bcc
Kim <kimmie1998@gmail.com>
2022-12-15 02:34:37 +0100

    Add files via upload
    hallo tim x2

--- website-tele2/scripts/index.js
+++ website-tele2/scripts/index.js
@@ -1,7 +1,11 @@
-var menuButton = document.querySelector("header section > button");
+var menuButton = document.querySelector("header li button");
+var sideNav = document.querySelector("header nav > section");
 
 menuButton.addEventListener("click", menuButtonKlik);
 
 function menuButtonKlik() {
   menuButton.classList.toggle("menuOpen");
+  menuButton.classList.toggle("open");
+  sideNav.classList.toggle("open");
+  document.body.classList.toggle("open");
 }
